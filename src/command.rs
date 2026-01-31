60ac2682fbcc218f9ab230ed46dbcf7ee4dd3982
wayuto <4e07910@gmail.com>
2026-01-31 19:37:49 +0800

    feat: description about subcommands

--- src/command.rs
+++ src/command.rs
@@ -2,13 +2,17 @@ use clap::{Parser, Subcommand};
 
 #[derive(Subcommand)]
 pub enum Commands {
+    #[command(about = "Create a new project")]
     New {
         name: String,
         #[arg(short, long, default_value = "C")]
         language: String,
     },
+    #[command(about = "Run the project")]
     Run,
+    #[command(about = "Build the project")]
     Build,
+    #[command(about = "Clean the project")]
     Clean,
 }
 
